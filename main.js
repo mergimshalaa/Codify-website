0106a1d4d1d302e4603388134e2ce730fb75e177
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2022-12-14 12:59:40 +0100

    done countdown

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 const countdown = () => {
-    const countDate = new Date('Dec 10, 2023 00:00:00')
-    console.log(countDate);
+    const countDate = new Date('Dec 10, 2023 00:00:00').getTime()
+    const now = new Date().getTime();
+    const gap = countDate - now;
+
+    //how to get time to work.
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+
+    const textDay = Math.floor(gap / day);
+    const textHour = Math.floor((gap % day) / hour);
+    const textMinute = Math.floor((gap % hour) / minute);
+    const textSecond = Math.floor((gap % minute) / second);
+
+    document.querySelector('.day').innerText = textDay;
+    document.querySelector('.hour').innerText = textHour;
+    document.querySelector('.minutes').innerText = textMinute;
+    document.querySelector('.seconds').innerText = textSecond;
+
 };
 
-countdown();
+setInterval(countdown, 1000);
